65247e17f06833a1fcf6a52993ff7ab4d6b5d7f2
aw3some-dev <olumidesofayo@yahoo.co.uk>
2021-04-01 12:48:53 +0100

    Added Build Controls Component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+
+    // const transformedIngredients = Object.keys(props.ingredients)
+    //     .map( ingrKey => {
+    //         return [...Array(props.ingredients[ingrKey])].map( (_, i) => {
+    //             return <BurgerIngredient key={ingrKey + i} type={ingrKey} />
+    //         });
+    //     });
+
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map( ingrKey => {
+            return [...Array(props.ingredients[ingrKey])].map( (_, i) => {
+                return <BurgerIngredient key={ingrKey + i} type={ingrKey} />;
+            });
+        })
+        .reduce((acc, el) => {
+            return acc.concat(el);
+        }, []);
+
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients.</p>;
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            { transformedIngredients }
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+}
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
+import Burger from "../../components/Burger/Burger";
 
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...};
+    // }
 
     state = {
         ingredients: {
             salad: 1,
-            bacon: 1,
             cheese: 2,
-            meat: 2
+            meat: 2,
+            bacon: 1
         }
     };
 
@@ -21,7 +26,7 @@ class BurgerBuilder extends Component {
                 {/* <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div> */}
 
-                <div>Burger ingredients</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
